5fbb0aa0043f581bc21ad80a660051715d70f9b9
Your Name <p13216884@GWHXX653WIN019W.lec-admin.dmu.ac.uk>
2017-02-14 12:56:30 +0000

    Added longitude and latitude for map and fixed issue with left panel

--- dictionary/web/json/descriptions.js
+++ dictionary/web/json/descriptions.js
@@ -4,21 +4,24 @@ let jsonString = `
     "type":"noun",
     "description":"A form of programming based around the use of objects that encapsulate data into fields and provide methods of interacting with this data",
     "example":"images/Everitt.jpg",
-    "origin":"..."
+    "originLat":"37.7749",
+    "originLong":"122.4194"
   },
   {
     "name":"programming",
     "type":"adjective",
     "description":"The process of how a computer program is written",
     "example":"images/fania.gif",
-    "origin":"..."
+    "originLat":"37.9838",
+    "originLong":"23.7275"
   },
   {
     "name":"bug",
     "type":"noun",
     "description":"An unexpected or incorrect result that is created by an error, failure or fault in a computer program",
     "example":"images/attwood.jpg",
-    "origin":"..."
+    "originLat":"36.546296",
+    "originLong":"-78.6569"
   }
 ]
 `;
