e479e4dce4c7e620ca677cbaf3d9fad4d00f16a9
Georgia <p13216884@my365.dmu.ac.uk>
2017-02-16 21:05:40 +0000

    Added descriptions to the dictionary and displays them. Also fixed error so unknown words are not displayed

--- dictionary/web/json/descriptions.js
+++ dictionary/web/json/descriptions.js
@@ -3,7 +3,7 @@ let jsonString = `
     "name":"object-oriented",
     "type":"noun",
     "description":"A form of programming based around the use of objects that encapsulate data into fields and provide methods of interacting with this data",
-    "example":"images/Everitt.jpg",
+    "example": ["Java is an object-oriented language.", "Object-oriented languages work around the concept of data being encapsulated in objects."],
     "originLat":"37.7749",
     "originLong":"122.4194"
   },
@@ -11,7 +11,7 @@ let jsonString = `
     "name":"programming",
     "type":"adjective",
     "description":"The process of how a computer program is written",
-    "example":"images/fania.gif",
+    "example": ["I will be programming a calculator later."], 
     "originLat":"37.9838",
     "originLong":"23.7275"
   },
@@ -19,7 +19,7 @@ let jsonString = `
     "name":"bug",
     "type":"noun",
     "description":"An unexpected or incorrect result that is created by an error, failure or fault in a computer program",
-    "example":"images/attwood.jpg",
+    "example": ["There is a bug in this program causing an ArrayOutOfBounds exception", "There must be a bug in this code somewhere.", "I have found the software bug."],
     "originLat":"36.546296",
     "originLong":"-78.6569"
   }
