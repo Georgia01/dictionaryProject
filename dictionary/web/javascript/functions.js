6256b53cf0318ff2ba7567adfc59a5992b9869d1
Georgia <georgiaingham@hotmail.co.uk>
2017-03-06 17:38:37 +0000

    Fixed animation issue caused by display types being changed in the wrong order.

--- dictionary/web/javascript/functions.js
+++ dictionary/web/javascript/functions.js
@@ -1,68 +1,78 @@
-function myFunc(){
+var moveMaindOnce = 0;
+
+function search(){
 
     let searchInput = document.getElementById("searchInput").value;
     console.log(`searchInput: ${searchInput}`);
 
+    //see if the word exists. If it doesn't then index = -1
     let obj = JSON.parse(jsonString);
     let index = obj.findIndex(function(item, i){
         return item.name === searchInput;
     });
-
-    if (index !== -1) {
-        document.getElementById("wordDoesNotExist").style.display = "none";
     
-        expandMain();
-        let nameSection = document.getElementById("name");
-        let nameData = `${obj[index].name}`;
-        nameSection.innerHTML = nameData.charAt(0).toUpperCase() + nameData.slice(1); 
-
-        let typeSection = document.getElementById("type");
-        let typeData = "(" + `${obj[index].type}` + ")";
-        typeSection.innerHTML = typeData; 
-
-        let defintionDataSection = document.getElementById("definition");
-        let definitionData = `${obj[index].description}`;
-        defintionDataSection.innerHTML = definitionData; 
-
-        let exampleDataSection = document.getElementById("example");
-        let exampleData = obj[index].example;
-        let finalExampleOutput = "";
-       for (i of exampleData) {
-                finalExampleOutput += i + "</br></br>";
-        }
-        exampleDataSection.innerHTML = finalExampleOutput; 
-
+    document.getElementById("mainBody").style.display = "block";
+    if (index !== -1) {
+        document.getElementById("mainContent").style.display = "block";
+        document.getElementById("wordList").style.display = "none";
+        fillInDetails(obj, index);
         addMap();
     } else {
-        document.getElementById("wordDoesNotExist").style.display = "block";
-        document.getElementById("mainBody").style.display = "none";
-        document.getElementById("navigationArticle").style.display = "block";
+        document.getElementById("mainContent").style.display = "none";
+        document.getElementById("wordList").style.display = "block";
     }
+    expandMain();
     event.preventDefault();
 }
 
 function expandMain(){
-
     if (typeof window.innerWidth !== 'undefined') {
         viewportwidth = window.innerWidth;
     }
 
     if (viewportwidth >= 500) {
-        document.getElementById("topBanner").style.animation =  "movingHeader 2s 1 forwards";
-        document.getElementById("mainBody").style.animation =  "movingBody 2s 1 forwards";
+        if (moveMaindOnce === 0) {
+            document.getElementById("topBanner").style.animation =  "movingHeader 2s 1 forwards";
+            document.getElementById("mainBody").style.animation =  "movingBody 2s 1 forwards";
+            moveMaindOnce = moveMaindOnce + 1;
+        }
     }
     else {
         document.body.style.marginTop = "0px";
-        document.getElementById("h1").style.paddingTop = "0px";
+        document.getElementById("pageHeader").style.paddingTop = "0px";
     }
-    document.getElementById("mainBody").style.display = "block";
     event.preventDefault();
+}
+
+ function fillInDetails(obj, index) {
+
+    let nameSection = document.getElementById("name");
+    let nameData = `${obj[index].name}`;
+    nameSection.innerHTML = nameData.charAt(0).toUpperCase() + nameData.slice(1);
+
+    let typeSection = document.getElementById("type");
+    let typeData = "(" + `${obj[index].type}` + ")";
+    typeSection.innerHTML = typeData;
+
+    let defintionDataSection = document.getElementById("definition");
+    let definitionData = `${obj[index].description}`;
+    defintionDataSection.innerHTML = definitionData;
+
+    let exampleDataSection = document.getElementById("example");
+    let exampleData = obj[index].example;
+    let finalExampleOutput = "";
+
+    for (i of exampleData) {
+        finalExampleOutput += i + "</br></br>";
+    }
+    exampleDataSection.innerHTML = finalExampleOutput;
 }
 
  function addMap(){
     var googleMapApi = document.createElement("script");
     googleMapApi.setAttribute("async", "");
     googleMapApi.setAttribute("defer", "");
+    //calls the initMap() to actually create the map and position the marker 
     googleMapApi.setAttribute("src", "https://maps.googleapis.com/maps/api/js?key=AIzaSyAXlGIU84NMenpp9ni_L4kMo9HOq7uHMgo&callback=initMap");
     document.body.appendChild(googleMapApi);
 }
@@ -73,6 +83,7 @@ function initMap() {
     let index = obj.findIndex(function(item, i){
         return item.name === searchInput;
     });
+    
     let originLatData = `${obj[index].originLat}`;
     let originLongData = `${obj[index].originLong}`;
 
